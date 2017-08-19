86a2514be0dec9a50fa2f3860e7f2e129bd2a9d9
Anton Litvinenko <dev.litvinenko@gmail.com>
2017-08-19 11:38:17 +0300

    add changing active menu item on scroll

--- app/js/common.js
+++ app/js/common.js
@@ -28,7 +28,7 @@ var mySwiper = new Swiper ('.swiper-container', {
 	pagination: '.swiper-pagination',
 	paginationClickable: true,
 	paginationHide: false
-})
+});
 
 
 /*To top button*/
@@ -52,7 +52,7 @@ $('.main-menu a').on('click', function () {
 	var elementClick = $(this).attr('href');
 	var destination = $(elementClick).offset().top;
 
-	if ($(elementClick).length != 0) {
+	if ($(elementClick).length != 0 ) {
 		$('html, body').animate({ scrollTop: destination - 80 }, 800);
 	}
 
@@ -75,5 +75,49 @@ $(window).scroll(function() {
 	}
 });
 
+
+// Active menu on scroll
+var lastId,
+		topMenu = $('.main-menu'),
+		topMenuHeight = topMenu.outerHeight() + 40,
+		// All list items
+		menuItems = topMenu.find('a'),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function(){
+			var item = $($(this).attr('href'));
+			if (item.length) { return item; }
+		});
+
+//Bind click handler to menu items
+//so we can get a fancy scroll animation
+// menuItems.click(function(e){
+// 	var href = $(this).attr('href'),
+// 		offsetTop = href === '#' ? 0 : $(href).offset().top - topMenuHeight + 1;
+// 	$('html, body').stop().animate({ 
+// 			scrollTop: offsetTop
+// 	}, 300);
+// 	e.preventDefault();
+// });
+
+// Bind to scroll
+$(window).scroll(function(){
+	// Get container scroll position
+	var fromTop = $(this).scrollTop() + topMenuHeight;
+	// Get id of current scroll item
+	var cur = scrollItems.map(function(){
+		if ($(this).offset().top < fromTop)
+			return this;
+	});
+	// Get the id of the current element
+	cur = cur[cur.length - 1];
+	var id = cur && cur.length ? cur[0].id : "";
+	if (lastId !== id) {
+			lastId = id;
+			// Set/remove active class
+			menuItems
+			.parent().removeClass('active')
+			.end().filter("[href='#" + id + "']").parent().addClass('active');
+	}
+});
 
 });
